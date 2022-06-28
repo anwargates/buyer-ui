a3a082c93f19a5d6d3474176c521efd6712a4004
anwargates <102929799+anwargates@users.noreply.github.com>
2022-06-28 23:27:23 +0700

    v012c
    connect api nyoba2

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import Buyer from './Pages/Buyer'
 import Seller from './Pages/Seller'
 import InfoPenawaran from './Pages/InfoPenawaran';
 
-
 const App = ()  => {
 
   return (
    <div className="app_container">
     <BrowserRouter>
      <Routes>
-       <Route path ="/" element={<Buyer/>}/>
+       <Route path ="/buyer/:id" element={<Buyer/>}/>
        <Route path ="/info" element={<InfoPenawaran/>}/>
        <Route path ="/seller" element={<Seller/>}/>
-
      </Routes>
      </BrowserRouter>
    </div>
